7a3a96a71d95b38d485d519aaed83b2a30900b7f
Joel Ward <joelward@cedarville.edu>
2024-09-07 16:28:35 -0400

    Initial Commit

---
+++ Bucket/BucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketApp.swift
+//  Bucket
+//
+//  Created by Joel Ward on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
